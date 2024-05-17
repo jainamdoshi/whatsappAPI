2d02f37b42b71aaf4b91618a2bd51a2426a5d9ce
jainamdoshi <jainam.doshi2001@gmail.com>
2024-05-17 08:54:31 +0400

    change localhost port to 8080

--- packages/api/src/config/init.ts
+++ packages/api/src/config/init.ts
@@ -3,6 +3,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const WHATSAPP_VERIFY_TOKEN = process.env.WHATSAPP_VERIFY_TOKEN || '';
+
+export const PORT = parseInt(process.env.PORT as string) || 8080;
+
 export const DB_HOST = process.env.DB_HOST || 'localhost';
 export const DB_PORT = parseInt(process.env.DB_PORT as string) || 5432;
 export const DB_DATABASE = process.env.DB_DATABASE || 'postgres';
--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -3,15 +3,15 @@ import bodyParser from 'body-parser';
 import whatsAppRouter from './routes/whatsapp';
 import { getDB } from './config/database';
 import { User, users } from './db/schema/users';
+import { PORT } from './config/init';
 
 const app = express().use(bodyParser.json());
-const port = 3000;
 
 app.use('/whatsapp', whatsAppRouter);
 
-app.listen(port, async () => {
+app.listen(PORT, async () => {
 	const db = await getDB();
 	const res: User[] = await db.select().from(users);
 	console.log(res);
-	console.log(`Server is running on http://localhost:${port}`);
+	console.log(`Server is running on http://localhost:${PORT}`);
 });
