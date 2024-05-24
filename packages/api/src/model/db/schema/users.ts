6845b21cb7f821235d1aa2066d4baa319c3723f3
jainamdoshi <jainam.doshi2001@gmail.com>
2024-05-24 23:26:31 +0400

    add name to user

--- packages/api/src/model/db/schema/users.ts
+++ packages/api/src/model/db/schema/users.ts
@@ -2,6 +2,7 @@ import { pgTable, serial, varchar } from 'drizzle-orm/pg-core';
 
 export const users = pgTable('users', {
 	id: serial('id').primaryKey(),
+	name: varchar('name', { length: 100 }).notNull(),
 	phoneNumber: varchar('phone_number', { length: 15 }).notNull().unique()
 });
 
