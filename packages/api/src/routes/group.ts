07208976e25e2f2a06b6590910bb4bf8a55f6642
jainamdoshi <jainam.doshi2001@gmail.com>
2024-06-08 02:42:28 +0400

    add get group by id route

--- packages/api/src/routes/group.ts
+++ packages/api/src/routes/group.ts
@@ -1,15 +1,32 @@
 import { Router } from 'express';
 import { Request, Response } from 'express';
 import { getGroup } from '../model/groups';
-import { parseGroups } from '../lib/group';
+import { NestedGroup, parseGroups } from '../lib/group';
+import { Group } from '../model/db/schema/groups';
 
 const groupRouter = Router();
 groupRouter.get('/', getGroups);
+groupRouter.get('/:groupId', getGroupById);
 
-async function getGroups(_: Request, res: Response) {
-	const groups = await getGroup();
+async function getGroups(_: Request, res: Response<NestedGroup[]>) {
+	const groups: Group[] = await getGroup();
 	const nestedGroups = parseGroups(groups);
 	return res.status(200).send(nestedGroups);
+}
+
+async function getGroupById(req: Request<{ groupId: string }, {}, {}, {}>, res: Response<NestedGroup | string>) {
+	const groupId = req.params.groupId;
+	const group: Group[] = await getGroup({ filter: { id: [parseInt(groupId)] } });
+
+	if (!group.length) {
+		return res.status(404).send(`Group with id ${groupId} not found`);
+	}
+
+	return res.status(200).send({
+		id: group[0].id,
+		name: group[0].name,
+		subGroup: []
+	});
 }
 
 export { groupRouter };
