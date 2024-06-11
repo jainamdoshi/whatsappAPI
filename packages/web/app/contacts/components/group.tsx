a3abc89463bc4c77001d7d75f7a9a08f1275d892
jainamdoshi <jainam.doshi2001@gmail.com>
2024-06-11 14:24:30 +0400

    make accordion trigger a link

--- packages/web/app/contacts/components/group.tsx
+++ packages/web/app/contacts/components/group.tsx
@@ -15,9 +15,9 @@ export default function ContactGroup({ group }: { group: Group }) {
 		<Accordion type='single' collapsible>
 			<AccordionItem value={group.id.toString()}>
 				<AccordionTrigger className='flex items-center justify-between px-4 py-2 text-sm font-medium text-gray-500 transition-colors hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50'>
-					<div>
+					<Link href={`/contacts/${group.id}`}>
 						{group.name} <span>({group.subGroup.length})</span>
-					</div>
+					</Link>
 				</AccordionTrigger>
 				<AccordionContent className='px-2'>
 					{group.subGroup.map((subGroup) => (
