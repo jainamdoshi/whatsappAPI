37494bbb58f2f6e49eff4876a190043860c56c8a
jainamdoshi <jainam.doshi2001@gmail.com>
2024-06-11 12:26:31 +0400

    Fix drizzle ssl error

--- packages/api/drizzle.config.ts
+++ packages/api/drizzle.config.ts
@@ -1,17 +1,11 @@
 import { defineConfig } from 'drizzle-kit';
-import { DB_HOST, DB_PORT, DB_DATABASE, DB_USER, DB_PASSWORD } from './src/config/init.ts';
+import { dbConfig } from './src/config/database';
 
 export default defineConfig({
 	schema: './src/model/db/schema',
 	dialect: 'postgresql',
 	out: './drizzle',
-	dbCredentials: {
-		host: DB_HOST,
-		port: DB_PORT,
-		user: DB_USER,
-		database: DB_DATABASE,
-		password: DB_PASSWORD
-	},
+	dbCredentials: dbConfig,
 	verbose: true,
 	strict: true
 });
--- packages/api/src/config/database.ts
+++ packages/api/src/config/database.ts
@@ -1,13 +1,17 @@
 import { drizzle, NodePgDatabase } from 'drizzle-orm/node-postgres';
 import { Client } from 'pg';
+import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER } from './init';
 
-const clientDB = new Client({
-	host: 'localhost',
-	port: 5432,
-	user: 'postgres',
-	password: 'adminadmin',
-	database: 'postgres'
-});
+export const dbConfig = {
+	host: DB_HOST,
+	port: DB_PORT,
+	user: DB_USER,
+	database: DB_DATABASE,
+	password: DB_PASSWORD,
+	ssl: false
+};
+
+const clientDB = new Client(dbConfig);
 
 let db: NodePgDatabase;
 
